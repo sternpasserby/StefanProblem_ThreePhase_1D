4e9e5a7c79d158cda47b835012f3a6686ce3251c
sternpasserby <sternpasserby@gmail.com>
2021-10-14 00:30:29 +0400

    Внедрил обезразмеривание

--- StefanProblemSolver.m
+++ StefanProblemSolver.m
@@ -17,11 +17,25 @@ Uf = pc.Uf;
 qf = pc.qf;
 rho = (rho1 + rho2)/2;
 alpha = bc.alpha;
-g0 = bc.g0;
-g3 = bc.g3;
-u1 = ic.u1;
-u2 = ic.u2;
-u3 = ic.u3;
+
+% Задание характерных параметров для обезразмеривания
+x0 = 1;
+t0 = rho1*c1*x0*x0/lambda1;
+U0 = Uf;
+beta = qf*rho / (rho1*c1*U0);
+kappa = c2*rho2/lambda2 * lambda1 / (c1*rho1); 
+
+% Обезразмеривание исходных данных
+g0 = @(t)(bc.g0(t*t0)/U0);
+g3 = @(t)(bc.g3(t*t0)/U0);
+u1 = ic.u1/U0;
+u2 = ic.u2/U0;
+u3 = ic.u3/U0;
+tau = tau/t0;
+tMax = tMax/t0;
+Uf = Uf/U0;
+alpha(1, 2) = bc.alpha(1, 2)/x0;
+alpha(2, 2) = bc.alpha(2, 2)/x0;
 
 N = Np - 1;
 h = 1/N;             % Шаг по координате
@@ -31,9 +45,10 @@ s1 = zeros(1, M + 1);
 s2 = zeros(1, M + 1);
 s3 = zeros(1, M + 1);
  t = zeros(1, M + 1);
-s1(1) = ic.s1;
-s2(1) = ic.s2;
-s3(1) = ic.s3;
+s0(1) = ic.s0/x0;
+s1(1) = ic.s1/x0;
+s2(1) = ic.s2/x0;
+s3(1) = ic.s3/x0;
 
 ksi = linspace(0, 1, Np)';
 A = zeros(Np);
@@ -62,14 +77,14 @@ for n = 1:M
     u3_past = u3;
     
     % Вычисление скоростей движения границ
-    C1 = lambda2/qf/rho/( s2(n) - s1(n) );
-    C2 = lambda1/qf/rho/( s1(n) - s0(n) );
+    C1 = 1/beta/( s1(n) - s0(n) );
+    C2 = lambda2/(lambda1*beta*( s2(n) - s1(n) ));
+    C3 = 1/beta/( s3(n) - s2(n) );
     ds0dt = 0;
-    ds1dt = C1/(2*h)*(-3*u2_past(1) + 4*u2_past(2) - u2_past(3)) - ...
-        C2/(2*h)*(3*u1_past(end) - 4*u1_past(end-1) + u1_past(end-2));
-    C2 = lambda1/qf/rho/( s3(n) - s2(n) );
-    ds2dt = C1/(2*h)*(3*u2_past(end) - 4*u2_past(end-1) + u2_past(end-2)) - ...
-        C2/(2*h)*(-3*u3_past(1) + 4*u3_past(2) - u3_past(3));
+    ds1dt = C2/(2*h)*(-3*u2_past(1) + 4*u2_past(2) - u2_past(3)) - ...
+        C1/(2*h)*(-u1_past(end) + 4*u1_past(end-1) - 3*u1_past(end-2));
+    ds2dt = C2/(2*h)*(-u2_past(end) + 4*u2_past(end-1) - 3*u2_past(end-2)) - ...
+        C3/(2*h)*(-3*u3_past(1) + 4*u3_past(2) - u3_past(3));
     ds3dt = 0;
     
     % Интегрирование уравнений движения границ
@@ -94,29 +109,30 @@ for n = 1:M
     end
     
     % Получение распределения тепла для первой фазы
-    [A, b] = getSysMat(u1_past, 1/a1_sq, tau, h, s1(n+1), s0(n + 1), ds1dt, ds0dt, ...
-       [alpha(1, :); htc -lambda1], g0(n*tau), htc*Uf);
+    [A, b] = getSysMat(u1_past, 1, tau, h, s1(n+1), s0(n + 1), ds1dt, ds0dt, ...
+       [alpha(1, :); 1 0], g0(n*tau), Uf);
 %     [A, b] = getSysMat(u1_past, 1/a1_sq, tau, h, s1(n+1), s0(n + 1), ds1dt, ds0dt, ...
 %        [alpha(1, :); 1 0], g0(n*tau), Uf);
     u1 = A \ b;
     
     % Получение распределения тепла для второй фазы
     if isThreePhase
-        [A, b] = getSysMat(u2_past, 1/a2_sq, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
-           [htc -lambda2; -htc -lambda2], htc*Uf, -htc*Uf);
+        [A, b] = getSysMat(u2_past, kappa, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
+           [htc -lambda2/x0; -htc -lambda2/x0], htc*Uf, -htc*Uf);
 %         [A, b] = getSysMat(u2_past, 1/a2_sq, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
 %            [1 0; 1 0], Uf, Uf);
         u2 = A \ b;
 
         % Получение распределения тепла для третьей фазы
-        [A, b] = getSysMat(u3_past, 1/a1_sq, tau, h, s3(n+1), s2(n+1), ds3dt, ds2dt, ...
-           [-htc -lambda1; -600 -lambda1], -htc*Uf, -600*g3(n*tau));
+        [A, b] = getSysMat(u3_past, 1, tau, h, s3(n+1), s2(n+1), ds3dt, ds2dt, ...
+           [1 0; -600 -lambda1/x0], Uf, -600*g3(n*tau));
 %         [A, b] = getSysMat(u3_past, 1/a1_sq, tau, h, s3(n+1), s2(n+1), ds3dt, ds2dt, ...
 %            [1 0; alpha(2, :)], Uf, g3(n*tau));
         u3 = A \ b;
     else
-        [A, b] = getSysMat(u2_past, 1/a2_sq, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
-           [htc -lambda2; alpha(2, :)], htc*Uf, g3(n*tau));
+        %ds2dt = C2/(2*h)*(-u2_past(end) + 4*u2_past(end-1) - 3*u2_past(end-2));
+        [A, b] = getSysMat(u2_past, kappa, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
+           [1 0; -600 -lambda1/x0], Uf, -600*g3(n*tau));
 %         [A, b] = getSysMat(u2_past, 1/a2_sq, tau, h, s2(n+1), s1(n+1), ds2dt, ds1dt, ...
 %             [1 0; alpha(2, :)], Uf, g3(n*tau));
         u2 = A \ b;
@@ -159,6 +175,14 @@ for n = 1:M
     end
 end
 s = [s0;s1;s2;s3];
+
+% Масшабирование к исходной размерности
+X = X*x0;
+T = T*t0;
+U = U*U0;
+s = s*x0;
+t = t*t0;
+
 elapsedTime = toc;
 fprintf("Elapsed time for Stefan Problem Solver: %4.2f sec.\n", elapsedTime);
 end
--- getSysMat.m
+++ getSysMat.m
@@ -1,6 +1,6 @@
 function [A, b] = getSysMat(u_past, kappa, tau, h, s_j, s_jm1, ...
     ds_jdt, ds_jm1dt, alpha, g_jm1, g_j)
-%GETSYSTEMMATRIX Вычисление матрицы системы для решения трёхфазной задачи
+%GETSYSTEMMATRIX Вычисление матрицы системы для решения задачи
 %Стефана методом конечных разностей с применением метода выпрямления
 %фронта. Граничные условия учитываются.
 %   u_past - распределение температур на предыдущем временном шаге
@@ -10,22 +10,22 @@ Np = length(u_past);
 A = zeros(Np);
 b = zeros(Np, 1);
 
-C3 = alpha(1, 2) / (s_j - s_jm1) / (2*h);
-A(1, 1) = alpha(1, 1) - 3*C3;
+C3 = alpha(1, 2)/(2*h);
+A(1, 1) = alpha(1, 1)*(s_j - s_jm1) - 3*C3;
 A(1, 2) = 4*C3;
 A(1, 3) = -C3;
-b(1) = g_jm1;
+b(1) = g_jm1*(s_j - s_jm1);
 for i = 2:Np-1
     C2 = kappa*(s_j - s_jm1)*( (1 - (i-1)*h)*ds_jm1dt + (i-1)*h*ds_jdt)/(2*h);
-    A(i, i) = -(2/h/h + C1);
     A(i, i - 1) = 1/h/h - C2;
+    A(i, i) = -(2/h/h + C1);
     A(i, i + 1) = 1/h/h + C2;
     b(i) = -C1*u_past(i);
 end
-C4 = alpha(2, 2) / (s_j - s_jm1)/(2*h);
+C4 = alpha(2, 2)/(2*h);
 A(end, end - 2) = C4;
 A(end, end - 1) = -4*C4;
-A(end, end) = 3*C4 + alpha(2, 1);
-b(end) = g_j;
+A(end, end) = 3*C4 + alpha(2, 1)*(s_j - s_jm1);
+b(end) = g_j*(s_j - s_jm1);
 end
 
